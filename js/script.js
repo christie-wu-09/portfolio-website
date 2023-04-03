cc211a7631f3d6f5050196850d6bf0c6fb4b27d3
Christie <127257937+christie-wu-09@users.noreply.github.com>
2023-04-02 20:32:34 -0700

    main page only

--- js/script.js
+++ js/script.js
@@ -58,6 +58,30 @@ function enlargeText() {
   textSize.style.fontSize = "60px";
 }
 
+//image carousal
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("img-slides");
+  if (n > x.length) {
+    slideIndex = 1;
+    console.log("image-slides");
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
 //draggable
 $(function () {
   $("#draggable").draggable();
